e0697e82c4a03fbe8e457b021e393d13755048cc
Marshanda <marshanda01@student.ciputra.ac.id>
2023-11-05 17:19:24 +0700

    DB Models Created

---
+++ app/Http/Controllers/KebaktianController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kebaktian;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreKebaktianRequest;
+use App\Http\Requests\UpdateKebaktianRequest;
+
+class KebaktianController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKebaktianRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Kebaktian $kebaktian)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Kebaktian $kebaktian)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKebaktianRequest $request, Kebaktian $kebaktian)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Kebaktian $kebaktian)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PersembahanController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Persembahan;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePersembahanRequest;
+use App\Http\Requests\UpdatePersembahanRequest;
+
+class PersembahanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePersembahanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Persembahan $persembahan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Persembahan $persembahan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePersembahanRequest $request, Persembahan $persembahan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Persembahan $persembahan)
+    {
+        //
+    }
+}
