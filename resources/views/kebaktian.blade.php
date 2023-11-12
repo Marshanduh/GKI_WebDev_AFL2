dca7959795b698188fd9bfa76f21cd16c0591de0
scotzy <smadlentsy@student.ciputra.ac.id>
2023-11-12 20:39:10 +0700

    responsive kebaktian

--- resources/views/kebaktian.blade.php
+++ resources/views/kebaktian.blade.php
@@ -4,17 +4,63 @@
     <style>
         .kebaktian {
             padding: 5px;
-            /* Sesuaikan dengan besar jarak yang diinginkan */
             margin-bottom: 5px;
-            /* Sesuaikan dengan besar jarak yang diinginkan */
         }
 
         .custom-image {
-
             width: 100%;
             height: 80%;
-
         }
+        .custom-hr{
+                border-top: 1px solid #ccc;
+                margin-top: 5px; 
+                margin-bottom: 5px; 
+            }
+
+        @media (max-width: 767px){
+            .kebaktian {
+                padding: 10px;
+                margin-bottom: 5px;
+            }
+
+            .jumbotron1 {
+                flex-direction: column;
+            }
+            .custom-image {
+                height: auto;
+            }
+
+            .display-4{
+                font-size: 20px;
+                font-weight: 500;
+            }
+
+            .lead {
+                font-size: 15px;
+                margin-bottom: 2px;
+            }
+
+            .my-4{
+                margin-top: 5px;
+            }
+
+            .capt{
+                font-size: small;
+                margin-bottom: 2px;
+            }
+
+            .btn-primary{
+                font-size: 10px;
+                padding: 5px 10px;
+            }
+
+            .custom-hr{
+                border-top: 1px solid #ccc;
+                margin-top: 5px; 
+            }
+            
+        }
+
     </style>
 
     <div class="card">
@@ -34,8 +80,8 @@
                             <div class="col">
                                 <h1 class="display-4">{{ $kebaktian->nama_ibadah }}</h1>
                                 <p class="lead">{{ $kebaktian->hari_pelaksanaan }}, {{ $kebaktian->waktu_ibadah }}</p>
-                                <hr class="my-4">
-                                <p>Lokasi kebaktian dilaksanakan di {{ $kebaktian->lokasi_kebaktian }}</p>
+                                <div class="custom-hr"></div>
+                                <p class="capt">Lokasi kebaktian dilaksanakan di {{ $kebaktian->lokasi_kebaktian }}</p>
                                 <a class="btn btn-primary btn-lg" href="#" role="button">Lokasi</a>
                             </div>
                         </div>
